15e9583f613f182bb6848db337cd4f89293794e7
jkuya <kuyajesee@proton.me>
2025-05-09 18:03:37 +0300

    feat: import model package and define user variable in register handler

--- backend/pkg/handler/register.go
+++ backend/pkg/handler/register.go
@@ -1,8 +1,14 @@
 package handler
 
-import "net/http"
+import (
+	"net/http"
+
+	"social/pkg/model"
+)
 
 func Register(w http.ResponseWriter, r *http.Request) {
+	var user model.User
+	
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
