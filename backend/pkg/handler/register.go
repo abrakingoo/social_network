5daa1b696afc6dad1d481a8581ad8b570b052434
jkuya <kuyajesee@proton.me>
2025-05-09 18:04:27 +0300

    feat: add JSON decoding for user input in register handler

--- backend/pkg/handler/register.go
+++ backend/pkg/handler/register.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"social/pkg/model"
@@ -8,9 +9,14 @@ import (
 
 func Register(w http.ResponseWriter, r *http.Request) {
 	var user model.User
-	
+
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest)
 		return
 	}
 }
