502be808c7f9804490ed554b695f08f1e178d5d7
jmuchiri <mmjames451@gmail.com>
2025-05-09 13:53:11 +0300

    update: change directory path for initializing the db and running the migration files

--- backend/pkg/db/db.go
+++ backend/pkg/db/db.go
@@ -4,19 +4,19 @@ import (
 	"database/sql"
 	"os"
 	"path/filepath"
-	"social/pkg/sqlite"
+
+	"social/pkg/db/sqlite"
 )
 
-
-func DBInstance () (*sql.DB ,error ){
+func DBInstance() (*sql.DB, error) {
 	currentDir, err := os.Getwd()
-    if err != nil {
-        return nil,err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    dbPath := filepath.Join(currentDir, "backend.db")
-    migrationsPath := filepath.Join(currentDir,"pkg","sqlite")
+	dbPath := filepath.Join(currentDir, "pkg", "db", "backend.db")
+	migrationsPath := filepath.Join(currentDir, "pkg", "db", "sqlite")
 
-    db, err := sqlite.InitDB(dbPath, migrationsPath)
-    return db,err
+	db, err := sqlite.InitDB(dbPath, migrationsPath)
+	return db, err
 }
