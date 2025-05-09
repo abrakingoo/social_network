6a4db02a0d4075edfbb363d3e556b17a7fafda66
jkuya <kuyajesee@proton.me>
2025-05-09 18:03:09 +0300

    feat: add method check to register handler

--- backend/pkg/handler/register.go
+++ backend/pkg/handler/register.go
@@ -2,4 +2,9 @@ package handler
 
 import "net/http"
 
-func Register(w http.ResponseWriter, r *http.Request) {}
+func Register(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+}
