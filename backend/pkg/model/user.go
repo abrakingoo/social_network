0486ead73e74c0559dcab854f9aba660f881646d
jkuya <kuyajesee@proton.me>
2025-05-09 14:28:53 +0300

    refactor: simplify User model JSON tags by removing GORM constraints

--- backend/pkg/model/user.go
+++ backend/pkg/model/user.go
@@ -3,16 +3,16 @@ package model
 import "time"
 
 type User struct {
-	ID                int       `json:"-" gorm:"primaryKey"`
-	Email             string    `json:"email" gorm:"uniqueIndex;not null"`
-	Password          string    `json:"password" gorm:"type:text;not null"`
-	ConfirmedPassword string    `json:"confirmed_password" gorm:"-"`
-	FirstName         string    `json:"first_name" gorm:"size:100;not null"`
-	LastName          string    `json:"last_name" gorm:"size:100;not null"`
-	DateOfBirth       time.Time `json:"date_of_birth" gorm:"type:date"`
-	Avatar            string    `json:"avatar" gorm:"type:text"`
-	Nickname          string    `json:"nickname" gorm:"size:50;uniqueIndex"`
-	AboutMe           string    `json:"about_me" gorm:"type:text"`
-	IsPublic          bool      `json:"is_public" gorm:"default:false"`
-	CreatedAt         time.Time `json:"created_at" gorm:"autoCreateTime"`
+	ID                int       `json:"-"`
+	Email             string    `json:"email"`
+	Password          string    `json:"password"`
+	ConfirmedPassword string    `json:"confirmed_password"`
+	FirstName         string    `json:"first_name"`
+	LastName          string    `json:"last_name"`
+	DateOfBirth       time.Time `json:"date_of_birth"`
+	Avatar            string    `json:"avatar"`
+	Nickname          string    `json:"nickname"`
+	AboutMe           string    `json:"about_me"`
+	IsPublic          bool      `json:"is_public"`
+	CreatedAt         time.Time `json:"created_at"`
 }
