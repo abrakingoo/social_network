2392cab7e80338035dc1821c3ef7181f239b26f8
jkuya <kuyajesee@proton.me>
2025-05-10 20:35:03 +0300

    refactor: rename sendErr utility to response and add Success function for JSON success responses

--- backend/pkg/util/response.go
+++ backend/pkg/util/response.go
@@ -13,3 +13,12 @@ func SendErr(w http.ResponseWriter, message string) {
 		"error": message,
 	})
 }
+
+// Success is a utility function to send a success response in JSON format
+func Success(w http.ResponseWriter, message string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": message,
+	})
+}
