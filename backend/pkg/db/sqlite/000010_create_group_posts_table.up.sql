c1554ea7612bd6cbeb01c0c6010aa3446822ac71
jmuchiri <mmjames451@gmail.com>
2025-05-10 16:42:06 +0300

    update: add new likes , dislikes and comments count to the post & comments , group posts and comments

--- backend/pkg/db/sqlite/000010_create_group_posts_table.up.sql
+++ backend/pkg/db/sqlite/000010_create_group_posts_table.up.sql
@@ -4,6 +4,9 @@ CREATE TABLE group_posts (
     user_id INTEGER NOT NULL,
     content TEXT,
     image TEXT,
+    likes_count INTEGER DEFAULT 0,  
+    dislikes_count INTEGER DEFAULT 0, 
+    comments_count INTEGER DEFAULT 0,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (group_id) REFERENCES groups(id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
