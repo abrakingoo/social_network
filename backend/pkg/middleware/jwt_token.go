b246d879cb20ad136ecd1de2450a79ba9c17eb1d
Aaron Ochieng <ochiengaoron@outlook.com>
2025-05-10 18:18:04 +0300

    Feat: JTW Token Validator - Validates a given token is valid or not - If the secret key is changed or modified, the jwttoken will be invalid - If the time has expired , the token will also be invalid - All in all the token must be in the correct format

--- backend/pkg/middleware/jwt_token.go
+++ backend/pkg/middleware/jwt_token.go
@@ -5,8 +5,10 @@ import (
 	"crypto/sha256"
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"social/pkg/utils"
+	"strings"
 	"time"
 )
 
@@ -27,7 +29,7 @@ func GenerateJWTToken(email, userID string) (string, error) {
 	encodedHeader := base64.RawURLEncoding.EncodeToString(headerJSON)
 
 	// Payload
-	payload := map[string]interface{}{
+	payload := map[string]any{
 		"sub":   userID,
 		"email": email,
 		"exp":   time.Now().Add(15 * time.Minute).Unix(),
@@ -49,4 +51,58 @@ func GenerateJWTToken(email, userID string) (string, error) {
 	// Final JWT
 	token := fmt.Sprintf("%s.%s.%s", encodedHeader, encodedPayload, signature)
 	return token, nil
+}
+
+func ValidateJWTToken(jwtToken string) (map[string]interface{}, error) {
+	secretKey, err := utils.GetEnvVal("SECRET_KEY")
+	if err != nil {
+		return nil, err
+	}
+	parts := strings.Split(jwtToken, ".")
+	if len(parts) != 3 {
+		return nil, errors.New("invalid token format")
+	}
+
+	// Decode Header
+	decodedHeader, err := base64.RawURLEncoding.DecodeString(parts[0])
+	if err != nil {
+		return nil, errors.New("invalid header encoding")
+	}
+
+	var header map[string]string
+	if err := json.Unmarshal(decodedHeader, &header); err != nil {
+		return nil, errors.New("invalid header JSON")
+	}
+
+	// Decode Payload
+	decodedPayload, err := base64.RawURLEncoding.DecodeString(parts[1])
+	if err != nil {
+		return nil, errors.New("invalid payload encoding")
+	}
+
+	var payload map[string]any
+	if err := json.Unmarshal(decodedPayload, &payload); err != nil {
+		return nil, errors.New("invalid payload JSON")
+	}
+
+	// Check Expiry
+	if exp, ok := payload["exp"].(float64); ok {
+		if time.Now().Unix() > int64(exp) {
+			return nil, errors.New("token has expired")
+		}
+	} else {
+		return nil, errors.New("no expiry in token")
+	}
+
+	// Verify Signature
+	signingInput := fmt.Sprintf("%s.%s", parts[0], parts[1])
+	h := hmac.New(sha256.New, []byte(secretKey))
+	h.Write([]byte(signingInput))
+	expectedSignature := base64.RawURLEncoding.EncodeToString(h.Sum(nil))
+
+	if parts[2] != expectedSignature {
+		return nil, errors.New("invalid signature")
+	}
+
+	return payload, nil
 }
