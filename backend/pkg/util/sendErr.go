8bb669fb46910f2c59f4bd17f36966538be9c483
jkuya <kuyajesee@proton.me>
2025-05-10 20:30:50 +0300

    feat: add utility function to send error responses in JSON format

---
+++ backend/pkg/util/sendErr.go
@@ -0,0 +1,15 @@
+package util
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// SendErr is a utility function to send an error response in JSON format
+func SendErr(w http.ResponseWriter, message string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusInternalServerError)
+	json.NewEncoder(w).Encode(map[string]string{
+		"error": message,
+	})
+}
