0f77570c987a85046bca6cd96a950a69ffb9ecda
aaochieng <ochiengaoron@outlook.com>
2025-05-13 14:09:49 +0300

    feat: add enum-based JSON response handling - Introduced the Message enum with string representations (Success, Error, Token). - Implemented the JSONResponse function to construct JSON responses using Message as the key. - Ensures type-safe and consistent handling of response message types.

--- backend/pkg/handler/json_response.go
+++ backend/pkg/handler/json_response.go
@@ -5,8 +5,20 @@ import (
 	"net/http"
 )
 
-func (app *App) JSONResponse(w http.ResponseWriter, r *http.Request, status int, message string) {
+type Message int
+
+const (
+	Success Message = iota
+	Error
+	Token
+)
+
+func (s Message) String() string {
+	return [...]string{"message", "error", "token"}[s]
+}
+
+func (app *App) JSONResponse(w http.ResponseWriter, r *http.Request, status int, message string, messageType Message) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-	json.NewEncoder(w).Encode(map[string]string{"message": message})
+	json.NewEncoder(w).Encode(map[string]string{messageType.String(): message})
 }
