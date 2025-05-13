0664e75798055b2924d5c85ba291da9711e83133
aaochieng <ochiengaoron@outlook.com>
2025-05-13 14:14:04 +0300

    feat: Add enum-based JSON response handling - Added message Enum as the key

--- backend/pkg/handler/login_handler.go
+++ backend/pkg/handler/login_handler.go
@@ -2,7 +2,6 @@ package handler
 
 import (
 	"encoding/base64"
-	"fmt"
 	"net/http"
 	"strings"
 
@@ -13,25 +12,25 @@ import (
 func (app *App) Login(w http.ResponseWriter, r *http.Request) {
 	authHeader := r.Header.Get("Authorization")
 	if authHeader == "" {
-		app.JSONResponse(w, r, http.StatusUnauthorized, "Unauthorized")
+		app.JSONResponse(w, r, http.StatusUnauthorized, "Unauthorized", Error)
 		return
 	}
 
 	if !strings.HasPrefix(authHeader, "Basic ") {
-		app.JSONResponse(w, r, http.StatusUnauthorized, "Unauthorized")
+		app.JSONResponse(w, r, http.StatusUnauthorized, "Unauthorized", Error)
 		return
 	}
 
 	payload, err := base64.StdEncoding.DecodeString(strings.TrimPrefix(authHeader, "Basic "))
 	if err != nil {
-		app.JSONResponse(w, r, http.StatusUnauthorized, "Unauthorized")
+		app.JSONResponse(w, r, http.StatusUnauthorized, "Unauthorized", Error)
 		return
 	}
 
 	// Split into email and password
 	parts := strings.SplitN(string(payload), ":", 2)
 	if len(parts) != 2 {
-		app.JSONResponse(w, r, http.StatusUnauthorized, "Unauthorized")
+		app.JSONResponse(w, r, http.StatusUnauthorized, "Unauthorized", Error)
 		return
 	}
 
@@ -40,23 +39,22 @@ func (app *App) Login(w http.ResponseWriter, r *http.Request) {
 	// Credentials validation
 	userId, encryptedPassword, err := app.Queries.GetUserCredentials(emailOrNickname)
 	if err != nil {
-		app.JSONResponse(w, r, http.StatusUnauthorized, "Unauthorized")
+		app.JSONResponse(w, r, http.StatusUnauthorized, "Unauthorized", Error)
 		return
 	}
 
 	// check password hash if it matches
 	if err := util.ValidatePassword(password, encryptedPassword); err != nil {
-		app.JSONResponse(w, r, http.StatusUnauthorized, "Unauthorized")
+		app.JSONResponse(w, r, http.StatusUnauthorized, "Unauthorized", Error)
 		return
 	}
 
 	// Generate jwt using userId and email/nickname
 	jwtToken, err := middleware.GenerateJWTToken(emailOrNickname, userId)
 	if err != nil {
-		app.JSONResponse(w, r, http.StatusInternalServerError, "Oops! Something went wrong")
-		fmt.Println(err)
+		app.JSONResponse(w, r, http.StatusInternalServerError, "Oops! Something went wrong", Error)
 		return
 	}
 
-	app.JSONResponse(w, r, http.StatusOK, jwtToken)
+	app.JSONResponse(w, r, http.StatusOK, jwtToken, Token)
 }
