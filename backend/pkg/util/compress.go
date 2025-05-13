c82aa86dfeb86c047c15fbab0070ba4bc203aaee
jkuya <kuyajesee@proton.me>
2025-05-13 22:03:05 +0300

    feat: Add PNG compression utility function

--- backend/pkg/util/compress.go
+++ backend/pkg/util/compress.go
@@ -3,7 +3,11 @@ package util
 import (
 	"errors"
 	"image"
+	"image/color"
+	"image/color/palette"
+	"image/gif"
 	"image/jpeg"
+	"image/png"
 	"os"
 	"path/filepath"
 )
@@ -32,4 +36,27 @@ func CompressJPEG(path string, quality int) error {
 	defer out.Close()
 
 	return jpeg.Encode(out, img, &jpeg.Options{Quality: quality})
+}
+
+// CompressPNG compresses PNG with the best available compression.
+func CompressPNG(path string) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return err
+	}
+
+	out, err := os.Create("compressed_" + filepath.Base(path))
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	encoder := png.Encoder{CompressionLevel: png.BestCompression}
+	return encoder.Encode(out, img)
 }
