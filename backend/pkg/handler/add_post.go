be32c1e15ca28a0e8f24cf104eee734ba07eacad
jkuya <kuyajesee@proton.me>
2025-05-14 21:32:20 +0300

    update: add file type validation in AddPost handler

--- backend/pkg/handler/add_post.go
+++ backend/pkg/handler/add_post.go
@@ -3,7 +3,10 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"strings"
+
+	"social/pkg/util"
 )
 
 func (app *App) AddPost(w http.ResponseWriter, r *http.Request) {
@@ -18,6 +21,19 @@ func (app *App) AddPost(w http.ResponseWriter, r *http.Request) {
 	if content == "" {
 		app.JSONResponse(w, r, http.StatusBadRequest, "Title and content cannot be empty", Error)
 		return
+	}
+
+	if file != "" {
+		f, err := os.Open(file)
+		if err != nil {
+			app.JSONResponse(w, r, http.StatusBadRequest, "Failed to open file", Error)
+			return
+		}
+		_, err = util.IsValidMimeType(f)
+		if err != nil {
+			app.JSONResponse(w, r, http.StatusBadRequest, "Invalid file type", Error)
+			return
+		}
 	}
 
 	fmt.Println("Content:", content)
