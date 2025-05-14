6138a7311c1446dc696b56cf7524e212274ffae5
jmuchiri <mmjames451@gmail.com>
2025-05-14 11:57:34 +0300

    "update: combine the post and group post, comments and group commnets to reference one table - delete the group post, group comments , group likes table and use the post logic to combine them to one table - delele the image field - rename migrations files to match the numerical order.

--- backend/pkg/db/sqlite/000003_create_comments_table.up.sql
+++ backend/pkg/db/sqlite/000003_create_comments_table.up.sql
@@ -2,11 +2,12 @@ CREATE TABLE IF NOT EXISTS comments (
     id TEXT PRIMARY KEY NOT NULL UNIQUE,
     post_id TEXT NOT NULL,
     user_id TEXT NOT NULL,
+    group_id TEXT,
     content TEXT,
-    image TEXT,
     likes_count INTEGER DEFAULT 0,  
     dislikes_count INTEGER DEFAULT 0, 
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (group_id) REFERENCES groups(id)
 );
