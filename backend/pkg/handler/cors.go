61d68a708a50205f640409fbb17535ebc5623370
josotione <jokumu25@gmail.com>
2025-05-16 00:04:31 +0300

    fix: improve OPTIONS method handling in CORS middleware

--- backend/pkg/handler/cors.go
+++ backend/pkg/handler/cors.go
@@ -8,10 +8,10 @@ func (app *App) WithCORS(handler http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
+		if r.Method == http.MethodOptions {
+            w.WriteHeader(http.StatusOK)
+            return
+        }
 
 		handler.ServeHTTP(w, r)
 	})
