8d9417c27cfa1fc1f059f129e8d4eebe75f81bbd
jkuya <kuyajesee@proton.me>
2025-05-15 23:51:16 +0300

    feat: implement CORS middleware and remove legacy CORS utility

--- backend/pkg/util/cors.go
+++
@@ -1,9 +0,0 @@
-package util
-
-import "net/http"
-
-func EnableCORS(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-}
