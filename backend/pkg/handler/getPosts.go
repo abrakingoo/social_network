cc835c96991c3f6a19cc1d11e743119457768460
jkuya <jokumu25@gmail.com>
2025-05-16 23:50:34 +0300

    feat: update GetPosts handler to fetch all posts instead of posts with media

--- backend/pkg/handler/getPosts.go
+++ backend/pkg/handler/getPosts.go
@@ -1,10 +1,14 @@
 package handler
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func (app *App) GetPosts(w http.ResponseWriter, r *http.Request) {
-	posts, err := app.Queries.FetchPostWithMedia()
+	posts, err := app.Queries.FetchAllPosts()
 	if err != nil {
+		fmt.Println(err)
 		app.JSONResponse(w, r, http.StatusInternalServerError, "Error fetching posts", Error)
 		return
 	}
