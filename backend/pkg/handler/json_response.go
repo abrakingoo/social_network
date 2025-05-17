75576d407f9dd11f79fc2294719e7eee17d29833
jkuya <jokumu25@gmail.com>
2025-05-17 21:36:45 +0300

    feat: extend JSONResponse message types to include 'data'

--- backend/pkg/handler/json_response.go
+++ backend/pkg/handler/json_response.go
@@ -15,7 +15,7 @@ const (
 )
 
 func (s Message) String() string {
-	return [...]string{"message", "error", "token"}[s]
+	return [...]string{"message", "error", "token", "data"}[s]
 }
 
 func (app *App) JSONResponse(w http.ResponseWriter, r *http.Request, status int, message any, messageType Message) {
