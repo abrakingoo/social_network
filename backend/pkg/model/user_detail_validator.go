4ea99089802336f7452bc0d99309a5a3dc0a4c98
jkuya <kuyajesee@proton.me>
2025-05-27 16:28:24 +0300

    feat: enhance user detail validation with improved date format checks and additional fields

--- backend/pkg/model/user_detail_validator.go
+++ backend/pkg/model/user_detail_validator.go
@@ -1,33 +1,15 @@
-// Author: Aaron Ochieng
-// Created: 10 May 2025 9:48pm EAT
-// Description: ValidateUserDetails is a mehtods which validates user datails upon user registration
-// 				It returns an error, status code and map of form errors
-// 				It will be used in conjustion with JSONResponse function to jsonify responses
-//
-// 				i.e
-// 				....
-// 					jsonResponse, err := json.Marshal(form_errors)
-// 					if err != nil {
-// 					JSONResponse(w, r, http.StatusInternalServerError, "Failed to generate json response")
-// 						return
-// 					}
-// 					w.WriteHeader(http.StatusNotAcceptable)
-// 					w.Write(jsonResponse)
-// 				....
-//
-
 package model
 
 import (
 	"fmt"
 	"net/http"
-	"strconv"
 	"strings"
 	"time"
 
 	"social/pkg/util"
 )
 
+// ValidateUserDetails validates user details during registration
 func ValidateUserDetails(w http.ResponseWriter, r *http.Request, user *User) (int, map[string][]string, error) {
 	form_errors := map[string][]string{}
 	first_name := strings.TrimSpace(r.FormValue("first_name"))
@@ -37,54 +19,58 @@ func ValidateUserDetails(w http.ResponseWriter, r *http.Request, user *User) (in
 	confirm_password := strings.TrimSpace(r.FormValue("confirmed_password"))
 	date_of_birth := strings.TrimSpace(r.FormValue("date_of_birth"))
 	nickname := strings.TrimSpace(r.FormValue("nickname"))
+	about_me := strings.TrimSpace(r.FormValue("about"))
+	avatar := strings.TrimSpace(r.FormValue("avatar"))
 
-	year_of_birth, err := strconv.Atoi(date_of_birth)
+	dob, err := time.Parse("02/01/2006", date_of_birth)
 	if err != nil {
-		form_errors["year_of_birth"] = append(form_errors["year_of_birth"], "Invalid year of birth")
+		form_errors["date_of_birth"] = append(form_errors["date_of_birth"], "Invalid date format. Use DD/MM/YYYY")
+	} else if time.Since(dob).Hours() < 13*365.25*24 {
+		form_errors["date_of_birth"] = append(form_errors["date_of_birth"], "Must be 13+ years")
 	}
 
-	// firstName validation
 	if first_name == "" {
 		form_errors["first_name"] = append(form_errors["first_name"], "firstname is required")
 	}
 
-	// LastName validation
 	if last_name == "" {
 		form_errors["last_name"] = append(form_errors["last_name"], "laststname is required")
 	}
 
-	// email address validation
 	if email_address == "" {
 		form_errors["email"] = append(form_errors["email"], "email address is required")
 	} else if !util.ValidateEmail(email_address) {
 		form_errors["email"] = append(form_errors["email"], "invalid email address")
 	}
 
-	// nickname validation
 	if nickname != "" {
 		if err := util.ValidateNickname(user.Nickname); err != nil {
 			form_errors["nickname"] = append(form_errors["nickname"], err.Error())
 		}
 	}
 
-	// Year of birth validation to ensure the user is 18+ years old
-	current_year := time.Now().Year()
-	if current_year-year_of_birth < 18 {
-		form_errors["date_of_birth"] = append(form_errors["date_of_birth"], "Must be 18+ years")
-	}
-
-	// TODO password validation here
 	if password == "" {
 		form_errors["password"] = append(form_errors["password"], "Password is required")
 	} else if confirm_password == "" {
 		form_errors["confirmed_password"] = append(form_errors["confirmed_password"], "Confirm Password is required")
 	} else if password != confirm_password {
 		form_errors["password"] = append(form_errors["password"], "Passwords do not match")
+	} else if len(password) < 8 {
+		form_errors["password"] = append(form_errors["password"], "Password must be at least 8 characters long")
 	}
 
 	if len(form_errors) != 0 {
 		return http.StatusNotAcceptable, form_errors, fmt.Errorf("form errors")
 	}
+
+	user.FirstName = first_name
+	user.LastName = last_name
+	user.Email = email_address
+	user.Password = password
+	user.DateOfBirth = dob
+	user.Nickname = nickname
+	user.AboutMe = about_me
+	user.Avatar = avatar
 
 	return http.StatusOK, nil, nil
 }
