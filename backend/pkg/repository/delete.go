dbce696e2a870de3121e5e6e14494701b1663b7a
jkuya <kuyajesee@proton.me>
2025-05-29 02:43:37 +0300

    feat: enhance session deletion logic to remove expired sessions

--- backend/pkg/repository/delete.go
+++ backend/pkg/repository/delete.go
@@ -1,8 +1,10 @@
 package repository
 
+import "time"
+
 // DeleteSession deletes a session from the database using the session token.
 func (q *Query) DeleteSession(sessionToken string) error {
-	_, err := q.Db.Exec("DELETE FROM sessions WHERE session_token = ?", sessionToken)
+	_, err := q.Db.Exec("DELETE FROM sessions WHERE session_token = ? OR expires_at < ?", sessionToken, time.Now())
 	if err != nil {
 		return err
 	}
