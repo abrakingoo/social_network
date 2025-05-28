c665aedd159625ebe0ff76e495f39977a9a962cb
jkuya <kuyajesee@proton.me>
2025-05-29 01:43:34 +0300

    feat: enhance logout functionality with session validation and error handling

--- backend/pkg/handler/logout.go
+++ backend/pkg/handler/logout.go
@@ -3,5 +3,15 @@ package handler
 import "net/http"
 
 func (app *App) Logout(w http.ResponseWriter, r *http.Request) {
-	
+	sessionCookie, err := r.Cookie("session_id")
+	if err != nil {
+		app.JSONResponse(w, r, http.StatusUnauthorized, "Unauthorized: session cookie missing", Error)
+		return
+	}
+
+	_, err = app.Queries.FetchSessionUser(sessionCookie.Value)
+	if err != nil {
+		app.JSONResponse(w, r, http.StatusUnauthorized, "Unauthorized: session not found", Error)
+		return
+	}
 }
