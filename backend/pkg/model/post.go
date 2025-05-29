cd59757456118b2156438d3fb3e2079059e34fa7
jmuchiri <mmjames451@gmail.com>
2025-05-29 21:48:57 +0300

    update: add new creator field to the post and comments struct

--- backend/pkg/model/post.go
+++ backend/pkg/model/post.go
@@ -4,6 +4,7 @@ import "time"
 
 type Post struct {
 	ID            string    `json:"id"`
+	User          Creator   `json:"user"`
 	GroupID       string    `json:"group_id"`
 	Content       string    `json:"content"`
 	LikesCount    int       `json:"likes_count"`
@@ -18,8 +19,7 @@ type Post struct {
 type Comment struct {
 	ID            string    `json:"id"`
 	PostID        string    `json:"post_id"`
-	UserID        string    `json:"user_id"`
-	GroupID       string    `json:"group_id"`
+	User          Creator   `json:"user"`
 	Content       string    `json:"content"`
 	Media         []Media   `json:"media"`
 	LikesCount    int       `json:"likes_count"`
