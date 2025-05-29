66dc7dbbfbdf9edff6bd132378462cba5fe22ad3
jkuya <kuyajesee@proton.me>
2025-05-29 22:12:21 +0300

    fix: change creator_id type from INTEGER to TEXT in groups table

--- backend/pkg/db/sqlite/000004_create_groups_table.up.sql
+++ backend/pkg/db/sqlite/000004_create_groups_table.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS groups (
     id TEXT PRIMARY KEY NOT NULL UNIQUE,
     title TEXT NOT NULL,
     description TEXT,
-    creator_id INTEGER NOT NULL,
+    creator_id TEXT NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (creator_id) REFERENCES users(id) ON DELETE CASCADE
 );
