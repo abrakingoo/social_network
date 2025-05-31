bf2097d8545c4d89bed8ebe222039f5036f6d5b9
vinomondi <vincentomondi1002@gmail.com>
2025-05-31 12:29:11 +0300

    feat: add support for credentials in CORS headers

--- backend/pkg/handler/cors.go
+++ backend/pkg/handler/cors.go
@@ -7,6 +7,7 @@ func (app *App) WithCORS(handler http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
