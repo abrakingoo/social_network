cb985a4b23496f26490a5ea535600e7e534a7d72
jmuchiri <mmjames451@gmail.com>
2025-05-31 22:59:48 +0300

    feature: add new group details struct and group user structs to model group data

---
+++ backend/pkg/model/group_details.go
@@ -0,0 +1,25 @@
+package model
+
+import "time"
+
+type GroupData struct {
+	ID        string    `json:"id"`
+	Title     string    `json:"title"`
+	About     string    `json:"about"`
+	Creator   Creator   `json:"Creator"`
+	CreatedAt time.Time `json:"created_at"`
+	Posts     []Post    `json:"group_post"`
+	Members   []Creator `json:"members"`
+	Events    []Events  `json:"Events"`
+}
+
+type Events struct {
+	ID          string    `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Creator     Creator   `json:"creator"`
+	EventTime   time.Time `json:"event_time"`
+	CreatedAt   time.Time `json:"created_at"`
+	Attendees   []Creator `json:"attendees"`
+}
+
