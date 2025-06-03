c32d8be8cffb15e4fc9b6e974ce049a112223e22
jkuya <kuyajesee@proton.me>
2025-06-03 10:30:10 +0300

    feat: enhance GetGroupData handler to fetch group ID and data

--- backend/pkg/handler/getGroup.go
+++ backend/pkg/handler/getGroup.go
@@ -21,4 +21,17 @@ func (app *App) GetGroupData(w http.ResponseWriter, r *http.Request) {
 		app.JSONResponse(w, r, http.StatusBadRequest, "Invalid JSON data", Error)
 		return
 	}
+
+	id, err := app.Queries.FetchGroupId(groupTitle.Title, userID)
+	if err != nil {
+		app.JSONResponse(w, r, http.StatusConflict, "Error fetching group ID", Error)
+		return
+	}
+
+	groupData, err := app.Queries.FetchGroupData(id)
+	if err != nil {
+		app.JSONResponse(w, r, http.StatusNoContent, "Error fetching group data", Error)
+		return
+	}
+	app.JSONResponse(w, r, http.StatusOK, groupData, Success)
 }
