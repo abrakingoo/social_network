8769f2853f422ed9640456aafe5c1ed7f8795f05
jkuya <kuyajesee@proton.me>
2025-06-03 10:51:15 +0300

    feat: add comment to GetGroupData function to clarify its purpose

--- backend/pkg/handler/getGroup.go
+++ backend/pkg/handler/getGroup.go
@@ -9,6 +9,8 @@ type GroupTitle struct {
 	Title string `json:"title"`
 }
 
+// GetGroupData handles the request to fetch group data based on the group title.
+// It decodes the JSON request body to get the group title, fetches the group ID,
 func (app *App) GetGroupData(w http.ResponseWriter, r *http.Request) {
 	var groupTitle GroupTitle
 
