dc71753688bcda956b32ad2365df7fcd6d906c84
jkuya <kuyajesee@proton.me>
2025-06-04 00:42:04 +0300

    feat: add user authentication check and event insertion logic in AddEvent handler

--- backend/pkg/handler/addEvent.go
+++ backend/pkg/handler/addEvent.go
@@ -8,6 +8,12 @@ import (
 )
 
 func (app *App) AddEvent(w http.ResponseWriter, r *http.Request) {
+	userID, err := app.GetSessionData(r)
+	if err != nil {
+		app.JSONResponse(w, r, http.StatusUnauthorized, "unauthorized", Error)
+		return
+	}
+
 	event := &model.Events{}
 	if err := json.NewDecoder(r.Body).Decode(&event); err != nil {
 		app.JSONResponse(w, r, http.StatusBadRequest, "invalid request body", Error)
@@ -16,6 +22,21 @@ func (app *App) AddEvent(w http.ResponseWriter, r *http.Request) {
 
 	if event.Title == "" || event.EventTime.IsZero() {
 		app.JSONResponse(w, r, http.StatusBadRequest, "missing required fields", Error)
+		return
+	}
+
+	err = app.Queries.InsertData("events", []string{
+		"title",
+		"creator_id",
+		"group_id",
+		"event_time",
+	}, []any{
+		event.Title,
+		userID,
+		event.EventTime,
+	})
+	if err != nil {
+		app.JSONResponse(w, r, http.StatusConflict, "failed to add event", Error)
 		return
 	}
 
