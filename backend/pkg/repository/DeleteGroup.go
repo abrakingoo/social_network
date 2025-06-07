3aac90a8279d0a33e76dd19abf5d003d1db8d93e
jmuchiri <mmjames451@gmail.com>
2025-06-07 18:04:24 +0300

    refactor: add checks for database errors and logic errros in our queries

--- backend/pkg/repository/DeleteGroup.go
+++ backend/pkg/repository/DeleteGroup.go
@@ -1,53 +1,67 @@
 package repository
 
 import (
+	"errors"
 	"fmt"
 )
 
-func (q *Query) DeleteGroup(groupName , userid string) error {
+var (
+	ErrGroupNotFound = errors.New("group not found")
+	ErrUnauthorized  = errors.New("user not authorized")
+)
+
+func (q *Query) DeleteGroup(groupName, userId string) error {
 	// Check if group exists
 	if err := q.CheckIfGroupExist(groupName); err != nil {
-		return fmt.Errorf("errGroupNotFound: %w", err)
+		return err
 	}
-	
+
 	// check if user is the creator
-	if err := q.CheckIfUserIsGroupAdmin(groupName,userid);err != nil {
-		return fmt.Errorf("errUnathorized: %w", err)
+	if err := q.CheckIfUserIsGroupAdmin(groupName, userId); err != nil {
+		return err
 	}
 
 	// delete group
 	if err := q.deleteGroup(groupName); err != nil {
 		return err
 	}
-	
+
 	return nil
 }
 
 func (q *Query) CheckIfGroupExist(groupName string) error {
 	var exists bool
-    query := `SELECT EXISTS(SELECT 1 FROM groups WHERE title = ?)`
-    
-    err := q.Db.QueryRow(query, groupName).Scan(&exists)
-    if err != nil {
-        return fmt.Errorf("group does not exist")
-    }
+	query := `SELECT EXISTS(SELECT 1 FROM groups WHERE title = ?)`
 
-    return nil
-} 
+	err := q.Db.QueryRow(query, groupName).Scan(&exists)
+	if err != nil {
+		return fmt.Errorf("database error: %v", err)
+	}
 
-func (q *Query) CheckIfUserIsGroupAdmin(groupName, userid string) error {
-	var admin bool
-	query := `SELECT EXISTS(SELECT 1 FROM groups WHERE title = ? AND creator_id = ?)`
-	err := q.Db.QueryRow(query, groupName, userid).Scan(&admin)
-    if err != nil {
-        return fmt.Errorf("user is not the admin")
-    }
+	if !exists {
+		return ErrGroupNotFound
+	}
 
 	return nil
 }
 
-func (q *Query) deleteGroup(groupname string) error {
-	_, err := q.Db.Exec("DELETE FROM groups WHERE title = ?", groupname)
+func (q *Query) CheckIfUserIsGroupAdmin(groupName, userId string) error {
+	var admin bool
+	query := `SELECT EXISTS(SELECT 1 FROM groups WHERE title = ? AND creator_id = ?)`
+	err := q.Db.QueryRow(query, groupName, userId).Scan(&admin)
+	if err != nil {
+		return fmt.Errorf("database err: %v", err)
+	}
+
+	if !admin {
+		return ErrUnauthorized
+	}
+
+	return nil
+}
+
+func (q *Query) deleteGroup(groupName string) error {
+	_, err := q.Db.Exec("DELETE FROM groups WHERE title = ?", groupName)
 	if err != nil {
 		return err
 	}
