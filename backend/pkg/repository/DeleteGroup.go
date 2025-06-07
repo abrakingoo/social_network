09e3bad5fe3d6aac49443c1e8740742fcfb77c67
jmuchiri <mmjames451@gmail.com>
2025-06-07 17:10:38 +0300

    update: add function to check if user is the creator of a group

--- backend/pkg/repository/DeleteGroup.go
+++ backend/pkg/repository/DeleteGroup.go
@@ -9,9 +9,13 @@ func (q *Query) DeleteGroup(groupName , userid string) error {
 	if err := q.CheckIfGroupExist(groupName); err != nil {
 		return fmt.Errorf("errGroupNotFound: %w", err)
 	}
-
+	
 	// check if user is the creator
+	if err := q.CheckIfUserIsGroupAdmin(groupName,userid);err != nil {
+		return fmt.Errorf("errUnathorized: %w", err)
+	}
 
+	
 	// delete group
 	
 	return nil
@@ -28,3 +32,14 @@ func (q *Query) CheckIfGroupExist(groupName string) error {
 
     return nil
 } 
+
+func (q *Query) CheckIfUserIsGroupAdmin(groupName, userid string) error {
+	var admin bool
+	query := `SELECT EXISTS(SELECT 1 FROM groups WHERE title = ? AND creator_id = ?)`
+	err := q.Db.QueryRow(query, groupName, userid).Scan(&admin)
+    if err != nil {
+        return fmt.Errorf("user is not the admin")
+    }
+
+	return nil
+}
