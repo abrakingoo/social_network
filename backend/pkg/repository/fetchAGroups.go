cc51367dc3c0114e4b2b259c40542f1566003272
jmuchiri <mmjames451@gmail.com>
2025-06-07 16:35:14 +0300

    feat: add functionality to check for user role and user status in a particular group

--- backend/pkg/repository/fetchAGroups.go
+++ backend/pkg/repository/fetchAGroups.go
@@ -1,13 +1,13 @@
 package repository
 
 import (
-	"social/pkg/model"
 	"fmt"
+	"log"
+	"social/pkg/model"
+	"database/sql"
 )
-	
 
-
-func (q *Query) FetchAllGroups(userid string)([]model.Groups, error) {
+func (q *Query) FetchAllGroups(userid string) ([]model.Groups, error) {
 	var groups []model.Groups
 	query := `
 		SELECT 
@@ -26,29 +26,42 @@ func (q *Query) FetchAllGroups(userid string)([]model.Groups, error) {
 		var group model.Groups
 
 		err := rows.Scan(
-			&group.ID, &group.Title, &group.About, &group.Creator.ID, &group.Creator.FirstName, &group.Creator.LastName, &group.Creator.Nickname, &group.Creator.Avatar, 
+			&group.ID, &group.Title, &group.About, &group.Creator.ID, &group.Creator.FirstName, &group.Creator.LastName, &group.Creator.Nickname, &group.Creator.Avatar,
 			&group.CreatedAt, &group.MembersCount,
 		)
 
 		if err != nil {
 			return nil, fmt.Errorf("failed to scan group: %w", err)
 		}
+		// check for user role in the group if present in the group
+		q.checkUserMembershipInGroup(userid, &group)
 
 		groups = append(groups, group)
 
 	}
 
-	
 	return groups, nil
 }
 
+func (q *Query) checkUserMembershipInGroup(userId string, group *model.Groups) {
+	var role string
+	var is_joined bool
 
-func (q *Query) checkUserMembershipInGroup(userId string, groupId string, group *model.Groups) bool {
+	query := `SELECT role FROM group_members WHERE user_id = ? AND group_id = ?`
 
-	return false
-}
+	err := q.Db.QueryRow(query, userId, group.ID).Scan(&role)
+	if err == sql.ErrNoRows {
+		// user does not exist
+		is_joined = false
+		role = ""
+	} else if err != nil {
+		// other DB error
+		log.Printf("CheckuserMemberShipInGroup: the db error is %v", err)
+		return
+	} else {
+		is_joined = true
+	}
 
-func (q *Query) checkUserRoleInGroup() string {
-
-	return "member"
+	group.IsJoined = is_joined
+	group.UserRole = role
 }
