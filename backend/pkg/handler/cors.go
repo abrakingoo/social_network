173ceb8b216762bf931548fd9a671b368119c2c9
Aaron Ochieng <ochiengaoron@outlook.com>
2025-06-09 22:17:37 +0300

    fix: added allowed origin [ included https ]

--- backend/pkg/handler/cors.go
+++ backend/pkg/handler/cors.go
@@ -5,6 +5,7 @@ import "net/http"
 func (app *App) WithCORS(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "https://localhost:3000")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
