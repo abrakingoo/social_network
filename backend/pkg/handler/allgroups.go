b6f2221d8b2bd07619bcad5ab38fab6a55fe6345
jkuya <kuyajesee@proton.me>
2025-06-09 20:47:27 +0300

    feat: add session validation to GetAllGroups handler

--- backend/pkg/handler/allgroups.go
+++ backend/pkg/handler/allgroups.go
@@ -3,4 +3,9 @@ package handler
 import "net/http"
 
 func (app *App) GetAllGroups(w http.ResponseWriter, r *http.Request) {
+	userID, err := app.GetSessionData(r)
+	if err != nil {
+		app.JSONResponse(w, r, http.StatusUnauthorized, "Unauthorized", Error)
+		return
+	}
 }
