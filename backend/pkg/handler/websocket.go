a6e3f7db5a3bbd9e7a216b4c3530564bc457a84d
jkuya <kuyajesee@proton.me>
2025-06-10 04:24:06 +0300

    feat: implement WebSocket handler for client connections

---
+++ backend/pkg/handler/websocket.go
@@ -0,0 +1,42 @@
+// handler/ws.go
+package handler
+
+import (
+	"log"
+	"net/http"
+
+	socket "social/pkg/websocket"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
+func (app *App) ServeWS(w http.ResponseWriter, r *http.Request) {
+	userID, err := app.GetSessionData(r)
+	if err != nil {
+		app.JSONResponse(w, r, http.StatusUnauthorized, "unauthorized", Error)
+		return
+	}
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("WebSocket upgrade failed:", err)
+		return
+	}
+	client := &socket.Client{
+		UserID:      userID,
+		Conn:        conn,
+		Send:        make(chan []byte, 256),
+		ProcessChan: make(chan map[string]string, 100),
+		Hubb:        app.Hub,
+	}
+
+	app.Hub.Register <- client
+	go client.WritePump()
+	go client.ProcessMessages()
+	client.ReadPump()
+}
