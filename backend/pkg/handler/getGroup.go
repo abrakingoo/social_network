285b2523dd44f36d49662d496ec995d1cabf7763
Vincent-Omondi <vincentomondi1002@gmail.com>
2025-06-11 01:25:25 +0300

    refactor: simplify GetGroupData handler to retrieve group title from query parameters

--- backend/pkg/handler/getGroup.go
+++ backend/pkg/handler/getGroup.go
@@ -1,26 +1,19 @@
 package handler
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
-type GroupTitle struct {
-	Title string `json:"title"`
-}
-
 // GetGroupData handles the request to fetch group data based on the group title.
-// It decodes the JSON request body to get the group title, fetches the group ID,
 func (app *App) GetGroupData(w http.ResponseWriter, r *http.Request) {
-	var groupTitle GroupTitle
-
-	err := json.NewDecoder(r.Body).Decode(&groupTitle)
-	if err != nil {
-		app.JSONResponse(w, r, http.StatusBadRequest, "Invalid JSON data", Error)
+	// Get title from query parameters
+	groupTitle := r.URL.Query().Get("title")
+	if groupTitle == "" {
+		app.JSONResponse(w, r, http.StatusBadRequest, "Title parameter is required", Error)
 		return
 	}
 
-	id, err := app.Queries.FetchGroupId(groupTitle.Title)
+	id, err := app.Queries.FetchGroupId(groupTitle)
 	if err != nil {
 		app.JSONResponse(w, r, http.StatusConflict, "Error fetching group ID", Error)
 		return
