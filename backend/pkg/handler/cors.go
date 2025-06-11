ac1529b8af37d243c7ad3f1c2256eb8bb121bcc2
Aaron Ochieng <ochiengaoron@outlook.com>
2025-06-11 19:03:32 +0300

    fix: cors origin - Added two origins for testing and while hosting using caddy server

--- backend/pkg/handler/cors.go
+++ backend/pkg/handler/cors.go
@@ -1,14 +1,21 @@
 package handler
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 func (app *App) WithCORS(handler http.Handler) http.Handler {
+	allowedOrigins := []string{"http://localhost", "http://localhost:8000"}
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-		w.Header().Set("Access-Control-Allow-Origin", "https://localhost:3000")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		origin := r.Header.Get("Origin")
+		if isOriginAllowed(origin, allowedOrigins) {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+		}
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
@@ -17,4 +24,13 @@ func (app *App) WithCORS(handler http.Handler) http.Handler {
 
 		handler.ServeHTTP(w, r)
 	})
+}
+
+func isOriginAllowed(origin string, allowedOrigins []string) bool {
+	for _, o := range allowedOrigins {
+		if strings.EqualFold(o, origin) {
+			return true
+		}
+	}
+	return false
 }
