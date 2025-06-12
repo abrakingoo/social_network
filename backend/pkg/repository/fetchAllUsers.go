775be20594d934727357aeb3611911f8f10497d7
jmuchiri <mmjames451@gmail.com>
2025-06-12 13:01:58 +0300

    feature: fetch userdata from the backend

---
+++ backend/pkg/repository/fetchAllUsers.go
@@ -0,0 +1,45 @@
+package repository
+
+import(
+	"social/pkg/model"
+	"fmt"
+	"log"
+) 
+
+
+func (q *Query) FetchAllUsers() ([]model.User ,error) {
+	var users []model.User
+	
+	query := `
+		SELECT 
+			u.id, u.first_name, u.last_name, u.nickname, u.avatar,  
+			u.is_public
+		FROM users u
+	`
+	rows, err := q.Db.Query(query)
+	if err != nil {
+		log.Printf("FetchAllUsers: db error: %v", err)
+		return nil, fmt.Errorf("failed to fetch users: %w", err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var user model.User
+
+		err := rows.Scan(
+			&user.ID, &user.FirstName, &user.LastName, &user.Nickname, 
+			&user.Avatar, &user.IsPublic,
+		)
+
+		if err != nil {
+			return nil, fmt.Errorf("failed to scan group: %w", err)
+		}
+
+		users = append(users, user)
+
+	}
+
+	return users, nil
+
+}
