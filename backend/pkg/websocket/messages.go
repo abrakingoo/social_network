628c5ef7de378c48b84fd2cb8b0512d964d30b71
jkuya <kuyajesee@proton.me>
2025-06-12 23:28:56 +0300

    call the method that handles follow requests incase of type follow request

--- backend/pkg/websocket/messages.go
+++ backend/pkg/websocket/messages.go
@@ -11,6 +11,8 @@ func (c *Client) ProcessMessages(q *repository.Query) {
 	for msg := range c.ProcessChan {
 		log.Printf("User %s sent: %v", c.UserID, msg)
 		switch msg["type"] {
+		case "follow_request":
+			c.FollowRequest(msg, q)
 		default:
 			c.SendError("Unknown message type")
 		}
