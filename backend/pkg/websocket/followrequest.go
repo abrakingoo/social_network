5068fa6318b9e143fa4592d3ced03af69a139b4d
jkuya <kuyajesee@proton.me>
2025-06-12 23:28:17 +0300

    feat: add a method to handler follow requests

---
+++ backend/pkg/websocket/followrequest.go
@@ -0,0 +1,55 @@
+package websocket
+
+import (
+	"encoding/json"
+
+	"social/pkg/model"
+	"social/pkg/repository"
+	"social/pkg/util"
+)
+
+func (c *Client) FollowRequest(msg map[string]any, q *repository.Query) {
+	dataBytes, err := json.Marshal(msg["data"])
+	if err != nil {
+		c.SendError("Invalid data encoding")
+		return
+	}
+
+	var request model.FollowRequest
+	if err := json.Unmarshal(dataBytes, &request); err != nil {
+		c.SendError("Invalid follow request data")
+		return
+	}
+
+	isPublic, err := q.CheckUserIsPublic(request.RecipientID)
+	if err != nil {
+		c.SendError("Error while checking user data")
+		return
+	}
+
+	if isPublic {
+		q.InsertData("user_follows", []string{
+			"id",
+			"follower_id",
+			"following_id",
+			"status",
+		}, []any{
+			util.UUIDGen(),
+			c.UserID,
+			request.RecipientID,
+			"accepted",
+		})
+	} else {
+		q.InsertData("user_follows", []string{
+			"id",
+			"follower_id",
+			"following_id",
+			"status",
+		}, []any{
+			util.UUIDGen(),
+			c.UserID,
+			request.RecipientID,
+			"pending",
+		})
+	}
+}
