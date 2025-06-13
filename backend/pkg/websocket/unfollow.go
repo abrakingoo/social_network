ea1f5d5b3e40001d33fbb75c7043054b4d8e21bd
jkuya <kuyajesee@proton.me>
2025-06-13 16:16:15 +0300

    feat: write the unfollowing logic

--- backend/pkg/websocket/unfollow.go
+++ backend/pkg/websocket/unfollow.go
@@ -1,5 +1,48 @@
 package websocket
 
-import "social/pkg/repository"
+import (
+	"encoding/json"
 
-func (c *Client) Unfollow(msg map[string]any, q *repository.Query) {}
+	"social/pkg/model"
+	"social/pkg/repository"
+)
+
+func (c *Client) Unfollow(msg map[string]any, q *repository.Query) {
+	dataBytes, err := json.Marshal(msg["data"])
+	if err != nil {
+		c.SendError("Invalid data encoding")
+		return
+	}
+
+	var request model.FollowRequest
+	if err := json.Unmarshal(dataBytes, &request); err != nil {
+		c.SendError("Invalid follow request data")
+		return
+	}
+
+	exists, status, err := q.FollowExists(request.RecipientID, c.UserID)
+	if err != nil {
+		c.SendError("Error while checking following status")
+		return
+	}
+
+	if !exists {
+		c.SendError("No follow request found.")
+		return
+	}
+
+	if status != "accepted" {
+		c.SendError("You don't follow this user")
+	}
+
+	err = q.DeleteData("user_follows", []string{
+		"follower_id",
+		"following_id",
+	}, []any{
+		c.UserID,
+		request.RecipientID,
+	})
+	if err != nil {
+		c.SendError("Error while deleting the follower")
+	}
+}
