3ce0634fb51d4bcea29929fb1d6ede6c625473ef
jkuya <kuyajesee@proton.me>
2025-06-13 23:12:13 +0300

    feat: add safety checks

--- backend/pkg/websocket/groupInvitations.go
+++ backend/pkg/websocket/groupInvitations.go
@@ -1,5 +1,33 @@
 package websocket
 
-import "social/pkg/repository"
+import (
+	"encoding/json"
 
-func (c *Client) SendInvitation(msg map[string]any, q *repository.Query) {}
+	"social/pkg/model"
+	"social/pkg/repository"
+)
+
+func (c *Client) SendInvitation(msg map[string]any, q *repository.Query) {
+	dataBytes, err := json.Marshal(msg["data"])
+	if err != nil {
+		c.SendError("Invalid data encoding")
+		return
+	}
+
+	var request model.GroupRequest
+	if err := json.Unmarshal(dataBytes, &request); err != nil {
+		c.SendError("Invalid follow request data")
+		return
+	}
+
+	admin, err := q.FetchGroupAdmin(request.GroupId)
+	if err != nil {
+		c.SendError("Error fetching group admin")
+		return
+	}
+
+	if admin != c.UserID {
+		c.SendError("Only group admin can send join invitations")
+		return
+	}
+}
