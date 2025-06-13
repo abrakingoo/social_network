a6e4e758f4ac13e116453aa5a25cc1fe1c870f34
jkuya <kuyajesee@proton.me>
2025-06-13 23:36:41 +0300

    feat: complete exit logic

--- backend/pkg/websocket/exitGroup.go
+++ backend/pkg/websocket/exitGroup.go
@@ -1,5 +1,62 @@
 package websocket
 
-import "social/pkg/repository"
+import (
+	"encoding/json"
 
-func (c *Client) ExitGroup(msg map[string]any, q *repository.Query) {}
+	"social/pkg/model"
+	"social/pkg/repository"
+)
+
+func (c *Client) ExitGroup(msg map[string]any, q *repository.Query) {
+	dataBytes, err := json.Marshal(msg["data"])
+	if err != nil {
+		c.SendError("Invalid data encoding")
+		return
+	}
+
+	var request model.GroupRequest
+	if err := json.Unmarshal(dataBytes, &request); err != nil {
+		c.SendError("Invalid follow request data")
+		return
+	}
+
+	admin, err := q.FetchGroupAdmin(request.GroupId)
+	if err != nil {
+		c.SendError("Error fetching group admin")
+		return
+	}
+
+	if admin == c.UserID {
+		c.SendError("Admins can't exit group")
+		return
+	}
+
+	isMember, err := q.CheckRow("group_members", []string{
+		"group_id",
+		"user_id",
+	}, []any{
+		request.GroupId,
+		c.UserID,
+	})
+	if err != nil {
+		c.SendError("error while checking user membership")
+		return
+	}
+
+	if !isMember {
+		c.SendError("Not a member of the group")
+		return
+	}
+
+	err = q.DeleteData("group_members", []string{
+		"group_id",
+		"user_id",
+	}, []any{
+		request.GroupId,
+		c.UserID,
+	})
+	if err != nil {
+		c.SendError("Failed to exit group. try again later")
+		return
+	}
+}
