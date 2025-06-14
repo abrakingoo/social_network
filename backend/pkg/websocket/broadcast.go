c2ae745c27a5dbda2f48e0d4861af09a0d67edac
jkuya <kuyajesee@proton.me>
2025-06-14 22:28:20 +0300

    feat: complete logic to broadcast to specific users

--- backend/pkg/websocket/broadcast.go
+++ backend/pkg/websocket/broadcast.go
@@ -25,4 +25,29 @@ func (h *Hub) BroadcastToOthers(skip *Client, payload map[string]any) {
 	}
 }
 
-func (h *Hub) BroadcastToSpecific(users []*Client, payload map[string]any) {}
+func (h *Hub) BroadcastToSpecific(users []string, payload map[string]any) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Println("specific broadcast error:", err)
+		return
+	}
+
+	for _, v := range users {
+		go h.SendIfActive(v, data)
+	}
+}
+
+func (h *Hub) SendIfActive(userID string, data []byte) {
+	h.Mu.RLock()
+	defer h.Mu.RUnlock()
+
+	for client := range h.Clients {
+		if client.UserID == userID {
+			select {
+			case client.Send <- data:
+			default:
+			}
+			return
+		}
+	}
+}
