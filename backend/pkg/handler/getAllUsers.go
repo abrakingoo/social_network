88b74463132cb2f6456b7073bb783144b149c241
jmuchiri <mmjames451@gmail.com>
2025-06-14 20:29:17 +0300

    update: get the userid and pass it to our repository function

--- backend/pkg/handler/getAllUsers.go
+++ backend/pkg/handler/getAllUsers.go
@@ -8,7 +8,13 @@ import (
 
 
 func (app *App) GetAllUsers(w http.ResponseWriter, r *http.Request) {
-	users, err := app.Queries.FetchAllUsers()
+	userID, err := app.GetSessionData(r)
+	if err != nil {
+		app.JSONResponse(w, r, http.StatusUnauthorized, "unathorized", Error)
+		return
+	}
+
+	users, err := app.Queries.FetchAllUsers(userID)
 	if err != nil {
 		app.JSONResponse(w, r, http.StatusNoContent, "Error fetching users", Error)
 		return
