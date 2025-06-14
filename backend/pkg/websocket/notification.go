c486b38045fb5311ac931c28513312c01ad17829
jkuya <kuyajesee@proton.me>
2025-06-14 22:50:01 +0300

    feat: complete action based notification logic

--- backend/pkg/websocket/notification.go
+++ backend/pkg/websocket/notification.go
@@ -1,5 +1,13 @@
 package websocket
 
-func (h *Hub) ActionBasedNotification(recipients []string, action string, data any) {}
+func (h *Hub) ActionBasedNotification(recipients []string, action string, data any) {
+	payload := map[string]any{
+		"type":        "notification",
+		"action_type": action,
+		"data":        data,
+	}
+
+	h.BroadcastToSpecific(recipients, payload)
+}
 
 func (h *Hub) InfoBasedNotification(recipients []string, message string) {}
