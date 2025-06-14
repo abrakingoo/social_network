647a7e16a7cad2f77456d149283a86e872710551
jmuchiri <mmjames451@gmail.com>
2025-06-14 20:31:44 +0300

    update: change our function to fetch users who are associated with the logged in user

--- backend/pkg/repository/fetchAllUsers.go
+++ backend/pkg/repository/fetchAllUsers.go
@@ -1,47 +1,60 @@
 package repository
 
-import(
-	"social/pkg/model"
+import (
 	"fmt"
 	"log"
-) 
+	"social/pkg/model"
+)
 
+func (q *Query) FetchAllUsers(userID string) ([]model.User, error) {
 
-func (q *Query) FetchAllUsers() ([]model.User ,error) {
-	var users []model.User
-	
+	return nil , nil
+}
+
+func (q *Query) fetchNonMutual(userID string, allUsers *model.AllUsers) error {
+
+	var users []model.Follower
 	query := `
 		SELECT 
 			u.id, u.first_name, u.last_name, u.nickname, u.avatar,  
 			u.is_public
 		FROM users u
+		WHERE u.id != ? 
+		AND NOT EXISTS (
+      		SELECT 1
+     			FROM user_follows uf
+      			WHERE 
+        		(uf.follower_id = u.id AND uf.following_id = ?) OR
+        		(uf.follower_id = ? AND uf.following_id = u.id)
+ 		)
 		ORDER BY u.created_at DESC
         LIMIT 100
 	`
-	rows, err := q.Db.Query(query)
+	rows, err := q.Db.Query(query, userID)
 	if err != nil {
 		log.Printf("FetchAllUsers: db error: %v", err)
-		return nil, fmt.Errorf("failed to fetch users: %w", err)
+		return fmt.Errorf("failed to fetch non mutuL users: %w", err)
 	}
 
 	defer rows.Close()
 
 	for rows.Next() {
-		var user model.User
+		var user model.Follower
 
 		err := rows.Scan(
-			&user.ID, &user.FirstName, &user.LastName, &user.Nickname, 
+			&user.ID, &user.FirstName, &user.Nickname,
 			&user.Avatar, &user.IsPublic,
 		)
 
 		if err != nil {
-			return nil, fmt.Errorf("failed to scan group: %w", err)
+			return fmt.Errorf("failed to scan user: %w", err)
 		}
 
 		users = append(users, user)
 
 	}
 
-	return users, nil
+	allUsers.NonMutual = users
 
+	return nil
 }
