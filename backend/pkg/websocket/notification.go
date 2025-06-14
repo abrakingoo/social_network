4d0614e917d74389afc9cae1d1203faf8fbe3b09
jkuya <kuyajesee@proton.me>
2025-06-14 22:46:19 +0300

    feat: declare ActionBasedNotification and InfoBasedNotification

---
+++ backend/pkg/websocket/notification.go
@@ -0,0 +1,5 @@
+package websocket
+
+func (h *Hub) ActionBasedNotification(recipients []string, action string, data any) {}
+
+func (h *Hub) InfoBasedNotification(recipients []string, message string) {}
