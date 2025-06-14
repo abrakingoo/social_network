315c0742c357d73b4661086f2ea51c71d9350d0d
jmuchiri <mmjames451@gmail.com>
2025-06-14 21:46:04 +0300

    chore: remove redundant code

--- backend/pkg/handler/getAllUsers.go
+++ backend/pkg/handler/getAllUsers.go
@@ -2,7 +2,6 @@ package handler
 
 import (
 	"net/http"
-	"social/pkg/model"
 
 )
 
@@ -19,11 +18,6 @@ func (app *App) GetAllUsers(w http.ResponseWriter, r *http.Request) {
 		app.JSONResponse(w, r, http.StatusNoContent, "Error fetching users", Error)
 		return
 	}
-
-	if len(users) == 0 {
-        app.JSONResponse(w, r, http.StatusOK, []model.User{}, Success)
-        return
-    }
 
 	app.JSONResponse(w, r, http.StatusOK, users, Success)
 }
