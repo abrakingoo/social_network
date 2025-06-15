a91e03e4f3946e0cb0f98d20466a0093e941104b
jkuya <kuyajesee@proton.me>
2025-06-16 02:56:13 +0300

    feat: declare the likePost method

--- backend/pkg/handler/likes.go
+++ backend/pkg/handler/likes.go
@@ -2,4 +2,11 @@ package handler
 
 import "net/http"
 
+type Like struct {
+	Id     string `json:"id"`
+	Status bool   `json:"status"`
+}
+
 func (app *App) LikeComment(w http.ResponseWriter, r *http.Request) {}
+
+func (app *App) LikePost(w http.ResponseWriter, r *http.Request) {}
