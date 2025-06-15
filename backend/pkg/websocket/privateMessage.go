0376e21fea31462a05b31cb8d8ee6cefd5eddcf1
jkuya <kuyajesee@proton.me>
2025-06-16 00:00:41 +0300

    feat: add security checks

--- backend/pkg/websocket/privateMessage.go
+++ backend/pkg/websocket/privateMessage.go
@@ -1,5 +1,28 @@
 package websocket
 
-import "social/pkg/repository"
+import (
+	"encoding/json"
+	"strings"
 
-func (c *Client) PrivateMessage(msg map[string]any, q *repository.Query, h *Hub) {}
+	"social/pkg/model"
+	"social/pkg/repository"
+)
+
+func (c *Client) PrivateMessage(msg map[string]any, q *repository.Query, h *Hub) {
+	data, err := json.Marshal(msg["data"])
+	if err != nil {
+		c.SendError("Invalid data encoding")
+		return
+	}
+
+	var private model.PrivateMessage
+	if err = json.Unmarshal(data, &private); err != nil {
+		c.SendError("Invalid messsage format")
+		return
+	}
+
+	if strings.TrimSpace(private.Message) == "" {
+		c.SendError("Cannot send empty message")
+		return
+	}
+}
