5ddd38c8596c98766651585fe61677774fa84567
jkuya <kuyajesee@proton.me>
2025-06-16 02:36:24 +0300

    refactor: format the code well and add security checks to the comments handler

--- backend/pkg/handler/comments.go
+++ backend/pkg/handler/comments.go
@@ -1,5 +1,32 @@
 package handler
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+)
 
-func (app *App) AddComment(w http.ResponseWriter, r *http.Request) {}
+type Comment struct {
+	PostId    string `json:"post_id"`
+	Content   string `json:"content"`
+	CommentId string `json:"comment_id"`
+}
+
+func (app *App) AddComment(w http.ResponseWriter, r *http.Request) {
+	userID, err := app.GetSessionData(r)
+	if err != nil {
+		app.JSONResponse(w, r, http.StatusUnauthorized, "unauthorized", Error)
+		return
+	}
+
+	comment := Comment{}
+	if err := json.NewDecoder(r.Body).Decode(&comment); err != nil {
+		app.JSONResponse(w, r, http.StatusBadRequest, "invalid request body", Error)
+		return
+	}
+
+	if strings.TrimSpace(comment.Content) == "" {
+		app.JSONResponse(w, r, http.StatusBadRequest, "Empty comment not allowed")
+		return
+	}
+}
--- backend/pkg/handler/getAllUsers.go
+++ backend/pkg/handler/getAllUsers.go
@@ -3,9 +3,7 @@ package handler
 import (
 	"net/http"
 	"social/pkg/model"
-
 )
-
 
 func (app *App) GetAllUsers(w http.ResponseWriter, r *http.Request) {
 	users, err := app.Queries.FetchAllUsers()
@@ -15,9 +13,9 @@ func (app *App) GetAllUsers(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if len(users) == 0 {
-        app.JSONResponse(w, r, http.StatusOK, []model.User{}, Success)
-        return
-    }
+		app.JSONResponse(w, r, http.StatusOK, []model.User{}, Success)
+		return
+	}
 
 	app.JSONResponse(w, r, http.StatusOK, users, Success)
 }
