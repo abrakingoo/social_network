85707e3e343a763c4b404c95badcb31e7390145c
jkuya <kuyajesee@proton.me>
2025-06-16 20:47:23 +0300

    feat: whole like post logic

--- backend/pkg/handler/likes.go
+++ backend/pkg/handler/likes.go
@@ -99,4 +99,89 @@ func (app *App) LikeComment(w http.ResponseWriter, r *http.Request) {
 	app.JSONResponse(w, r, http.StatusOK, "Like status updated successfully", Error)
 }
 
-func (app *App) LikePost(w http.ResponseWriter, r *http.Request) {}
+func (app *App) LikePost(w http.ResponseWriter, r *http.Request) {
+	userID, err := app.GetSessionData(r)
+	if err != nil {
+		app.JSONResponse(w, r, http.StatusUnauthorized, "Unauthorized", Error)
+		return
+	}
+
+	like := Like{}
+
+	if err = json.NewDecoder(r.Body).Decode(&like); err != nil {
+		app.JSONResponse(w, r, http.StatusBadRequest, "invalid request body", Error)
+		return
+	}
+
+	if like.PostId == "" {
+		app.JSONResponse(w, r, http.StatusBadRequest, "No post provided", Error)
+		return
+	}
+
+	exist, err := app.Queries.CheckRow("post_likes", []string{
+		"post_id",
+		"user_id",
+	}, []any{
+		like.PostId,
+		userID,
+	})
+	if err != nil {
+		app.JSONResponse(w, r, http.StatusConflict, "Error while checking like status", Error)
+		return
+	}
+
+	if exist {
+		status := true
+		isTrue, err := app.Queries.CheckRow("post_likes", []string{
+			"post_id",
+			"user_id",
+			"is_like",
+		}, []any{
+			like.PostId,
+			userID,
+			true,
+		})
+		if err != nil {
+			app.JSONResponse(w, r, http.StatusConflict, "Error while checking like status", Error)
+			return
+		}
+
+		if isTrue {
+			status = false
+		}
+
+		err = app.Queries.UpdateData("post_likes", []string{
+			"post_id",
+			"user_id",
+		}, []any{
+			like.PostId,
+			userID,
+		}, []string{
+			"is_like",
+		}, []any{
+			status,
+		})
+		if err != nil {
+			app.JSONResponse(w, r, http.StatusInternalServerError, "Failed to update like status", Error)
+			return
+		}
+	} else {
+		err = app.Queries.InsertData("post_likes", []string{
+			"id",
+			"post_id",
+			"user_id",
+			"is_like",
+		}, []any{
+			util.UUIDGen(),
+			like.PostId,
+			userID,
+			true,
+		})
+		if err != nil {
+			app.JSONResponse(w, r, http.StatusInternalServerError, "Failed to update like status", Error)
+			return
+		}
+	}
+
+	app.JSONResponse(w, r, http.StatusOK, "Like status updated successfully", Error)
+}
