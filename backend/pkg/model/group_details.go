0d6b1bd7cfdbfdb2c4832c1da29f06c132525bbb
jmuchiri <mmjames451@gmail.com>
2025-06-16 23:58:43 +0300

    chore: rename struct

--- backend/pkg/model/group_details.go
+++ backend/pkg/model/group_details.go
@@ -3,15 +3,15 @@ package model
 import "time"
 
 type GroupData struct {
-	ID          string    `json:"id"`
-	Title       string    `json:"title"`
-	About       string    `json:"about"`
-	Creator     Creator   `json:"Creator"`
-	CreatedAt   time.Time `json:"created_at"`
-	Posts       []Post    `json:"group_post"`
-	Members     []Creator `json:"members"`
-	Events      []Events  `json:"Events"`
-	JoinRequest []Request `jsont:"join_request"`
+	ID          string             `json:"id"`
+	Title       string             `json:"title"`
+	About       string             `json:"about"`
+	Creator     Creator            `json:"Creator"`
+	CreatedAt   time.Time          `json:"created_at"`
+	Posts       []Post             `json:"group_post"`
+	Members     []Creator          `json:"members"`
+	Events      []Events           `json:"Events"`
+	JoinRequest []GroupJoinRequest `jsont:"join_request"`
 }
 
 type Events struct {
@@ -38,7 +38,7 @@ type Groups struct {
 	UserRole     string    `json:"user_role"`
 }
 
-type Request struct {
+type GroupJoinRequest struct {
 	ID        string      `json:"id"`
 	UserID    string      `json:"user_id"`
 	User      UserSummary `json:"user"`
