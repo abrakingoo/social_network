1ea5e91089c7b934357e08805c162a7a947cb362
jkuya <kuyajesee@proton.me>
2025-06-16 03:15:20 +0300

    feat: add security checks

--- backend/pkg/websocket/groupCancel.go
+++ backend/pkg/websocket/groupCancel.go
@@ -1,5 +1,54 @@
 package websocket
 
-import "social/pkg/repository"
+import (
+	"encoding/json"
 
-func (c *Client) CancelGroupInvitation(msg map[string]any, q *repository.Query) {}
+	"social/pkg/model"
+	"social/pkg/repository"
+)
+
+func (c *Client) CancelGroupInvitation(msg map[string]any, q *repository.Query) {
+	dataBytes, err := json.Marshal(msg["data"])
+	if err != nil {
+		c.SendError("Invalid data encoding")
+		return
+	}
+
+	var request model.GroupRequest
+	if err := json.Unmarshal(dataBytes, &request); err != nil {
+		c.SendError("Invalid follow request data")
+		return
+	}
+
+	admin, err := q.FetchGroupAdmin(request.GroupId)
+	if err != nil {
+		c.SendError("Error fetching group admin")
+		return
+	}
+
+	if admin != c.UserID {
+		c.SendError("Only group admin can cancel join invitations")
+		return
+	}
+
+	exists, err := q.CheckRow("group_invitations", []string{
+		"group_id",
+		"receiver_id",
+		"sender_id",
+		"status",
+	}, []any{
+		request.GroupId,
+		request.RecipientID,
+		c.UserID,
+		"pending",
+	})
+	if err != nil {
+		c.SendError("Error while checking invitations")
+		return
+	}
+
+	if !exists {
+		c.SendError("This invitation was not found")
+		return
+	}
+}
