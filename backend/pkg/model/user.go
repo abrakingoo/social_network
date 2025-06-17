36b5cb3e3354b6f9f48ff3994518f9113eda67e5
jmuchiri <mmjames451@gmail.com>
2025-06-17 12:55:01 +0300

    feature: add usernotification struct

--- backend/pkg/model/user.go
+++ backend/pkg/model/user.go
@@ -16,3 +16,24 @@ type User struct {
 	IsPublic          bool      `json:"is_public"`
 	CreatedAt         time.Time `json:"created_at"`
 }
+
+type UserNotification struct {
+	ID         string    `json:"id"`
+	ActorId    string    `json:"sender_id"`
+	Type       string    `json:"type"`
+	EntityID   string    `json:"entity_id,omitempty"`
+	EntityType string    `json:"entity_type,omitempty"`
+	IsRead     bool      `json:"is_read"`
+	Message    string    `json:"message"`
+	CreatedAt  time.Time `json:"created_at"`
+}
+
+// id TEXT PRIMARY KEY NOT NULL UNIQUE,
+//     recipient_id TEXT NOT NULL,
+//     actor_id TEXT,
+//     type TEXT NOT NULL,
+//     entity_id INTEGER,
+//     entity_type TEXT,
+//     is_read BOOLEAN DEFAULT 0,
+//     message TEXT,
+//     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
