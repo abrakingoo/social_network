67442240caeec97e662e15f4f54ad81197178908
jkuya <kuyajesee@proton.me>
2025-06-17 21:59:37 +0300

    feat: add the notification route with its accepted http methods

--- backend/pkg/handler/routes_handler.go
+++ backend/pkg/handler/routes_handler.go
@@ -29,6 +29,23 @@ var allowedRoutes = map[string][]string{
 	"/api/addComment":   {"POST", "OPTIONS"},
 	"/api/likeComment":  {"POST", "OPTIONS"},
 	"/api/likePost":     {"POST", "OPTIONS"},
+	"/api/login":         {"POST", "OPTIONS"},
+	"/api/register":      {"POST", "OPTIONS"},
+	"/api/addPost":       {"POST", "OPTIONS"},
+	"/api/getPosts":      {"GET", "OPTIONS"},
+	"/api/profile":       {"GET", "OPTIONS"},
+	"/api/logout":        {"POST", "OPTIONS"},
+	"/api/addGroup":      {"POST", "OPTIONS"},
+	"/api/getGroupData":  {"GET", "POST", "OPTIONS"},
+	"/pkg/db/media/":     {"GET", "OPTIONS"},
+	"/api/addEvent":      {"POST", "OPTIONS"},
+	"/api/updateUser":    {"PATCH", "OPTIONS"},
+	"/api/groups":        {"GET", "OPTIONS"},
+	"/api/deleteGroup":   {"DELETE", "OPTIONS"},
+	"/api/ws":            {"GET", "OPTIONS"},
+	"/api/rsvp":          {"POST", "OPTIONS"},
+	"/api/users":         {"GET", "OPTIONS"},
+	"/api/notifications": {"GET", "OPTIONS"},
 }
 
 type App struct {
@@ -96,6 +113,7 @@ func (app *App) Routes() http.Handler {
 	mux.Handle("/api/addComment", app.AuthMiddleware(http.HandlerFunc(app.AddComment)))
 	mux.Handle("/api/likeComment", app.AuthMiddleware(http.HandlerFunc(app.LikeComment)))
 	mux.Handle("/api/likePost", app.AuthMiddleware(http.HandlerFunc(app.LikePost)))
+	mux.Handle("/api/notifications", app.AuthMiddleware(http.HandlerFunc(app.Notifications)))
 
 	return mux
 }
