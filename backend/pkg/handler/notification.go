a87ec3266e9e47cb991e8981237b18ce31153a54
jkuya <kuyajesee@proton.me>
2025-06-17 21:54:20 +0300

    feat: fetch user id from the cookie.

--- backend/pkg/handler/notification.go
+++ backend/pkg/handler/notification.go
@@ -2,4 +2,10 @@ package handler
 
 import "net/http"
 
-func (app *App) Notifications(w http.ResponseWriter, r *http.Request) {}
+func (app *App) Notifications(w http.ResponseWriter, r *http.Request) {
+	userID, err := app.GetSessionData(r)
+	if err != nil {
+		app.JSONResponse(w, r, http.StatusUnauthorized, "Unauthorized", Error)
+		return
+	}
+}
