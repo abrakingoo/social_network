9147be55005e88c09bd8160bf794ec9b763f62c2
Vincent-Omondi <vincentomondi1002@gmail.com>
2025-06-19 20:09:58 +0300

    feat: add delete_notification message handling in WebSocket for client notifications

--- backend/pkg/websocket/messages.go
+++ backend/pkg/websocket/messages.go
@@ -45,6 +45,8 @@ func (c *Client) ProcessMessages(q *repository.Query, h *Hub) {
 			c.LoadPrivateMessages(msg, q)
 		case "load_group_messages":
 			c.LoadGroupMessages(msg, q)
+		case "delete_notification":
+			c.DeleteNotification(msg, q)
 		default:
 			c.SendError("Unknown message type")
 		}
