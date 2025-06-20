93ccb4645173954ccfba017bc70f89f4b355f687
JosephOkumu <jokumu25@gmail.com>
2025-06-20 15:41:17 +0300

    feat: redirect followers tab to followers route

--- frontend/src/app/profile/page.js
+++ frontend/src/app/profile/page.js
@@ -283,7 +283,11 @@ const Profile = () => {
             <TabsTrigger value="about" className="flex-1">
               About
             </TabsTrigger>
-            <TabsTrigger value="followers" className="flex-1">
+            <TabsTrigger
+              value="followers"
+              className="flex-1"
+              onClick={() => router.push("/followers?tab=followers")}
+            >
               Followers
             </TabsTrigger>
             <TabsTrigger value="photos" className="flex-1">
