be4923c5e7a2d23b3c2b63772f9a3c699ee8bcd5
jkuya <kuyajesee@proton.me>
2025-06-27 17:40:16 +0300

    feat(profile): implement GetProfile handler with session validation and request body decoding

--- backend/pkg/handler/getProfile.go
+++ backend/pkg/handler/getProfile.go
@@ -1,5 +1,26 @@
 package handler
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
-func (app *App) GetProfile(w http.ResponseWriter, r *http.Request) {}
+type UserData struct {
+	UserID string `json:"user_id"`
+}
+
+func (app *App) GetProfile(w http.ResponseWriter, r *http.Request) {
+	userID, err := app.GetSessionData(r)
+	if err != nil {
+		fmt.Println(err)
+		app.JSONResponse(w, r, http.StatusUnauthorized, "Getpost: unathorized ", Error)
+		return
+	}
+
+	queryId := UserData{}
+	if err := json.NewDecoder(r.Body).Decode(&queryId); err != nil {
+		app.JSONResponse(w, r, http.StatusBadRequest, "invalid request body", Error)
+		return
+	}
+}
